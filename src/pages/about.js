920b3454b0c13527d5b34f6031022ff1ef3ac356
Kishoren1 <kishorenaskar1@gmail.com>
2023-03-03 12:02:19 +0530

    admin link added

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,6 +6,18 @@ export default function About() {
     <Layout>
       <div className="container py-2">
         <h2>Hello about</h2>
+        <p className="py-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod
+          reprehenderit eos non aut ipsa reiciendis, vitae doloremque architecto
+          unde, quis iusto impedit sit dolore doloribus, tenetur dignissimos
+          nemo optio quasi velit nam! Perferendis eum nihil officia voluptates
+          quae magnam provident ratione dolor possimus. Dolor nihil dolorem
+          delectus mollitia sunt enim!
+        </p>
+
+        <a href="https://server-nine-coral.vercel.app/desk" target="_blank">
+          Admin
+        </a>
       </div>
     </Layout>
   );
